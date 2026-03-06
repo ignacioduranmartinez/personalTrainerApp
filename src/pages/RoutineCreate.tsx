29f9c7172c6fd078d1bc6c7678a941cc4d97074d
ignacioduranmartinez <ignacioduranm@gmail.com>
2026-03-06 07:58:01 +0100

    Add files via upload

--- src/pages/RoutineCreate.tsx
+++ src/pages/RoutineCreate.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useMemo, useState } from 'react'
+import { useMemo, useState } from 'react'
 import { useNavigate } from 'react-router-dom'
 import { saveRoutine } from '../lib/routineDb'
 import type { RoutineImportJSON, Exercise, DaySchedule, WeekSchedule } from '../types/routine'
